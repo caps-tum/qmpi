6b9871a651393649d1581827b1731dc7a73b5378
bengisuelis <bengisu.elis@tum.de>
2018-07-24 10:34:20 +0200

    no message

--- lib/comp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mpicc  -std=c99 -fPIC -rdynamic -shared  -o  ./T1.so ./T1.c  ../src/obj/qmpi.c  
-mpicc  -std=c99 -fPIC -rdynamic -shared  -o  ./T3.so ./T3.c  ../src/obj/qmpi.c 
-mpicc  -std=c99 -fPIC -rdynamic -shared  -o  ./T2.so ./T2.c  ../src/obj/qmpi.c
-cd mpiP && make clean && make shared --include=../../src/obj/qmpi.h && cd ../ 
